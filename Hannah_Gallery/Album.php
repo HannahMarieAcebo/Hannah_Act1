5784909695443f26f72de0fffa6f5319db952618
liezelbael <jessryll.cadorna@evsu.edu.ph>
2021-11-09 14:18:02 +0800

    first commit

---
+++ Hannah_Gallery/Album.php
@@ -0,0 +1,46 @@
+<link rel="stylesheet" type="text/css" href="style.css">
+<div id="dvPic">
+	<img src="pictures/cp.jpg"> <br><br>
+	
+</div>
+<div id="dvPic">
+	<img src="pictures/cp3.jpg"> <br><br>
+	
+</div>
+<div id="dvPic">
+	<img src="pictures/cp4.jpg">  <br><br>
+	
+</div>
+<div id="dvPic">
+	<img src="pictures/cp5.jpg"> <br><br>
+	
+</div>
+<div id="dvPic">
+	<img src="pictures/cp6.jpg"> <br><br>
+	
+</div>
+<div id="dvPic">
+	<img src="pictures/cp7.jpg"> <br><br>
+	
+</div>
+<div id="dvPic">
+	<img src="pictures/cp8.jpg"> <br><br>
+	
+</div>
+<div id="dvPic">
+	<img src="pictures/cp9.jpg"> <br><br>
+
+</div>
+<div id="dvPic">
+	<img src="pictures/cp10.jpg">  <br><br>
+	
+</div>
+<div id="dvPic">
+	<img src="pictures/cp11.jpg"> <br><br>
+</div>
+<div id="dvPic">
+	<img src="pictures/cp12.jpg"> <br><br>
+</div>
+<div id="dvPic">
+	<img src="pictures/cp13.jpg"> <br><br>
+</div>
